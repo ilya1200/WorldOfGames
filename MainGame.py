02276de92855b8d73918dab35d8a66152158088d
ilya <ilya1200@gmail.com>
2021-11-25 20:15:44 +0200

    add basic logger

--- MainGame.py
+++ MainGame.py
@@ -1,5 +1,21 @@
+import os
+
 from Live import load_game, welcome
+import logging
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+PATH_TO_LOG_FILE = f'{ROOT_DIR}//Logs/wog.log'
+LOGGER_FORMAT = '[%(levelno)s %(levelname)s][%(asctime)s][%(process)d %(processName)s %(thread)d %(threadName)s][%(filename)s %(lineno)d]%(message)s'
+
+logging.basicConfig(filename=PATH_TO_LOG_FILE, level=logging.DEBUG,
+                    format=LOGGER_FORMAT)
 
 if __name__ == '__main__':
-    print(welcome("Ilya"))
+    PLAYER_NAME = "Ilya"
+    logging.debug(f"Player name {PLAYER_NAME}")
+
+    WELCOME_MESSAGE = welcome(PLAYER_NAME)
+    logging.info(WELCOME_MESSAGE)
+    print(WELCOME_MESSAGE)
     load_game()
+    logging.info("WoG Finished")
