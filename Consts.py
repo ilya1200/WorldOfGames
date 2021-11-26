14007e3d9c957977cbbd738eb9eb312087be32f6
ilya <ilya1200@gmail.com>
2021-11-26 17:26:56 +0200

    change logger file name

--- Consts.py
+++ Consts.py
@@ -1,5 +1,6 @@
 import os
+from datetime import datetime
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-PATH_TO_LOG_FILE = f'{ROOT_DIR}//Logs/wog.log'
+PATH_TO_LOG_FILE = f'{ROOT_DIR}//Logs/wog_{datetime.now().strftime("%a-%d-%m-%y_%H:%M:%S")}.log'
 LOGGING_FORMAT = '[%(levelno)s %(levelname)s][%(name)s][%(asctime)s][%(process)d %(processName)s %(thread)d %(threadName)s][%(filename)s %(funcName)s %(lineno)d]::\t%(message)s'
