cd97f40eb507a0e837fd30346a4694127f3b5d20
ilya <ilya1200@gmail.com>
2021-11-27 18:52:31 +0200

    add test_get_games

--- tests/test_Live.py
+++ tests/test_Live.py
@@ -1,3 +1,5 @@
+from typing import List, Dict, Any
+
 import pytest
 
 from Live import Live
@@ -14,4 +16,15 @@ from Live import Live
 def test_welcome(name: str):
     expected_welcome_msg: str = f"Hello {name} and welcome to the World of Games (WoG).\nHere you can find many cool games to play."
     actual_welcome_msg: str = Live.welcome(name)
-    assert(actual_welcome_msg, expected_welcome_msg)  # add assertion here
+    assert (actual_welcome_msg, expected_welcome_msg)  # add assertion here
+
+
+def test_get_games():
+    expected_games: List[str] = ["Memory Game", "Guess Game", "Currency Roulette"]
+    games: List[Dict[str, Any]] = Live._get_games()
+
+    assert games is not None
+    assert type(games) == list
+
+    actual_games_list: List[str] = list(map(lambda game: game['name'], games))
+    assert actual_games_list == expected_games
