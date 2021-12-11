c8a163637bf8cb9165eb5198afcaf8ad1d436d11
ilya <ilya1200@gmail.com>
2021-12-11 15:39:08 +0200

    add test_add_score

--- tests/test_Score.py
+++ tests/test_Score.py
@@ -9,7 +9,10 @@ from Score.Score import Score
 
 
 class TestScore:
-    TEST_SCORE_FILE_PATH = Path(f"{ROOT_DIR}/tests/{Utils.SCORES_FILE_NAME}")
+    TEST_SCORE_FILE_PATH: Path = Path(f"{ROOT_DIR}/tests/{Utils.SCORES_FILE_NAME}")
+
+    def setup_class(self):
+        Score.SCORE_FILE_PATH = TestScore.TEST_SCORE_FILE_PATH
 
     @pytest.mark.parametrize(
         "score",
@@ -19,8 +22,6 @@ class TestScore:
             30
         ])
     def test_write_score_to_file(self, score: int):
-        Score.SCORE_FILE_PATH = TestScore.TEST_SCORE_FILE_PATH
-
         if os.path.exists(Score.SCORE_FILE_PATH):
             os.remove(Score.SCORE_FILE_PATH)
 
@@ -30,7 +31,7 @@ class TestScore:
             score_file_lines: list = score_file.readlines()
             assert_that(score_file_lines).is_not_empty()
 
-            first_line = score_file_lines[0].strip()
+            first_line: str = score_file_lines[0].strip()
             assert_that(first_line).is_not_empty()
             assert_that(first_line.isnumeric()).is_true()
             assert_that(int(first_line)).is_equal_to(score)
@@ -43,18 +44,42 @@ class TestScore:
             30
         ])
     def test_write_score_to_file_override(self, score: int):
-        Score.SCORE_FILE_PATH = TestScore.TEST_SCORE_FILE_PATH
-
         Score._write_score(score)
 
         with open(Score.SCORE_FILE_PATH, 'r') as score_file:
             score_file_lines: list = score_file.readlines()
             assert_that(score_file_lines).is_not_empty()
 
-            first_line = score_file_lines[0].strip()
+            first_line: str = score_file_lines[0].strip()
             assert_that(first_line).is_not_empty()
             assert_that(first_line.isnumeric()).is_true()
             assert_that(int(first_line)).is_equal_to(score)
+
+    @pytest.mark.parametrize(
+        "difficulty,current_score",
+        [
+            (10, 0),
+            (20, 35),
+            (30, 17)
+        ])
+    def test_add_score(self, difficulty: int, current_score: int):
+        EXPECTED_NEW_SCORE: int = current_score + (difficulty * 3) + 5
+
+        with open(Score.SCORE_FILE_PATH, 'w+') as score_file:
+            score_file.write(str(current_score))
+
+        Score.add_score(difficulty)
+
+        with open(Score.SCORE_FILE_PATH, 'r') as score_file:
+            score_file_lines: list = score_file.readlines()
+            assert_that(score_file_lines).is_not_empty()
+
+            first_line: str = score_file_lines[0].strip()
+            assert_that(first_line).is_not_empty()
+            assert_that(first_line.isnumeric()).is_true()
+
+            ACTUAL_NEW_SCORE: int = int(first_line)
+            assert_that(ACTUAL_NEW_SCORE).is_equal_to(EXPECTED_NEW_SCORE)
 
     def teardown_class(self):
         if os.path.exists(TestScore.TEST_SCORE_FILE_PATH):
