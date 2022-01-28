b1cf8fa8894bbf92612e727ca49e66cce9738cae
ilya1200 <ilya1200@gmail.com>
2022-01-28 11:31:08 +0200

    update e2e

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,48 +1,15 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
+import sys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+chromeOptions = webdriver.ChromeOptions()
+chromeOptions.add_argument("--no-sandbox")
+chromeOptions.add_argument("--disable-dev-shm-using")
+chromeOptions.add_argument("--disable-extensions")
+chromeOptions.add_argument(r"user-data-dir=.\cookies\\test")
+chromeOptions.headless = True
+chrome_driver = webdriver.Chrome(chrome_options=chromeOptions)
 
-TIME_TO_WAIT: int = 20
-
-locators = {
-    "score": (By.ID, "score"),
-}
-
-
-def chrome_driver() -> WebDriver:
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("no-sandbox")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--headless")
-    chrome_driver: WebDriver = webdriver.Chrome(chrome_options=chrome_options)
-    chrome_driver.implicitly_wait(TIME_TO_WAIT)
-    return chrome_driver
-
-
-def test_scores_service(app_url: str) -> bool:
-    driver: WebDriver = chrome_driver()
-    driver.get(app_url)
-    try:
-        score_element: WebElement = driver.find_element(*locators["score"])
-        score: int = int(score_element.text)
-        assert 1 <= score <= 1000
-    except (AssertionError, NoSuchElementException):
-        return False
-    else:
-        return True
-    finally:
-        driver.quit()
-
-
-def main_function():
-    APP_URL: str = "http://127.0.0.1:5001/"
-    is_pass = test_scores_service(APP_URL)
-    if is_pass:
-        exit(0)
-    exit(-1)
-
-
-if __name__ == '__main__':
-    main_function()
+chrome_driver.get('https://www.google.com')
+print(chrome_driver.title)
+chrome_driver.quit()
